5b7ab7fad2b211cfc34bd41295191863dd4fa139
Izzulhaq Fawwaz Syauqiy <57482751+IzzulhaqFS@users.noreply.github.com>
2023-02-19 18:46:10 +0700

    Add files via upload

---
+++ Astronomy-App/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+    }
+}
